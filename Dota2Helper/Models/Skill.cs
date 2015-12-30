7b234da8238364bf66b62be1aae32d0e2dd6346a
QuarK264 <alexey.danilov@hotmail.com>
2015-12-31 00:06:27 +0300

    Delete unusing usings in several files. Delete Приходит/блокируется

--- Dota2Helper/Models/Skill.cs
+++ Dota2Helper/Models/Skill.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Dota2Helper.Models
+﻿namespace Dota2Helper.Models
 {
     public class Skill
     {
